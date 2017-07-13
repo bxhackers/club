1da4b06ce885bbbf9f957e70fac6ac3c466a8f6a
Jack Cook <hello@jackcook.nyc>
2017-07-12 23:25:30 -0700

    Added logic for using the browser back button

--- links.js
+++ links.js
@@ -1,20 +1,31 @@
-var anchors = document.getElementsByTagName("a");
+function setupLinkHandling() {
+    window.history.pushState({"html": document.documentElement.outerHTML}, "", window.location.href);
 
-for (var i = 0; i < anchors.length; i++) {
-    var href = anchors[i].getAttribute("href");
+    var anchors = document.getElementsByTagName("a");
 
-    anchors[i].onclick = function() {
-        var href = this.href;
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementsByTagName("html")[0].innerHTML = this.responseText;
-                window.history.pushState({"html": this.responseText}, "", href);
-            }
+    for (var i = 0; i < anchors.length; i++) {
+        anchors[i].onclick = function() {
+            var href = this.href;
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    window.history.pushState({"html": this.responseText}, "", href);
+                    document.getElementsByTagName("html")[0].innerHTML = this.responseText;
+                    setupLinkHandling();
+                }
+            };
+            xhttp.open("GET", href, true);
+            xhttp.send();
+
+            return false;
         };
-        xhttp.open("GET", "./about/team.html", true);
-        xhttp.send();
+    }
 
-        return false;
+    window.onpopstate = function(e) {
+        if (e.state) {
+            document.getElementsByTagName("html")[0].innerHTML = e.state.html;
+        }
     };
 }
+
+setupLinkHandling();
