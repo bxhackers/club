8ee8ca9a567ea9c25a79a276992e119f737b5eef
Jack Cook <hello@jackcook.nyc>
2017-08-31 23:16:46 -0400

    Removed custom link handling

--- js/navigation.js
+++ js/navigation.js
@@ -1,43 +1,35 @@
-function setupNavigation() {
-    if (document.getElementById("navigation") == null) {
-        return;
-    }
-    
-    var anchors = document.getElementsByTagName("a");
-    var sub_navigation_bars = document.getElementsByClassName("sub-navigation");
+var anchors = document.getElementsByTagName("a");
+var sub_navigation_bars = document.getElementsByClassName("sub-navigation");
 
-    for (var i = 0; i < anchors.length; i++) {
-        if (anchors[i].parentElement.id == "main-navigation" && anchors[i].innerHTML != "Home") {
-            anchors[i].onmouseover = function(e) {
-                for (var j = 0; j < sub_navigation_bars.length; j++) {
-                    if (sub_navigation_bars[j].getAttribute("parent") == this.innerHTML) {
-                        sub_navigation_bars[j].className = "sub-navigation";
-                    } else {
-                        sub_navigation_bars[j].className = "sub-navigation hidden";
-                    }
+for (var i = 0; i < anchors.length; i++) {
+    if (anchors[i].parentElement.id == "main-navigation" && anchors[i].innerHTML != "Home") {
+        anchors[i].onmouseover = function(e) {
+            for (var j = 0; j < sub_navigation_bars.length; j++) {
+                if (sub_navigation_bars[j].getAttribute("parent") == this.innerHTML) {
+                    sub_navigation_bars[j].className = "sub-navigation";
+                } else {
+                    sub_navigation_bars[j].className = "sub-navigation hidden";
                 }
-            };
-        }
-    }
-
-    document.getElementById("navigation").onmouseleave = function(e) {
-        var selected_anchors = document.getElementsByClassName("selected");
-        var sub_navigation_bars = document.getElementsByClassName("sub-navigation");
-
-        for (var i = 0; i < selected_anchors.length; i++) {
-            if (selected_anchors[i].parentElement.id == "main-navigation") {
-                for (var j = 0; j < sub_navigation_bars.length; j++) {
-                    if (sub_navigation_bars[j].getAttribute("parent") == selected_anchors[i].innerHTML) {
-                        sub_navigation_bars[j].className = "sub-navigation";
-                    } else {
-                        sub_navigation_bars[j].className = "sub-navigation hidden";
-                    }
-                }
-
-                break;
             }
-        }
-    };
+        };
+    }
 }
 
-setupNavigation();
+document.getElementById("navigation").onmouseleave = function(e) {
+    var selected_anchors = document.getElementsByClassName("selected");
+    var sub_navigation_bars = document.getElementsByClassName("sub-navigation");
+
+    for (var i = 0; i < selected_anchors.length; i++) {
+        if (selected_anchors[i].parentElement.id == "main-navigation") {
+            for (var j = 0; j < sub_navigation_bars.length; j++) {
+                if (sub_navigation_bars[j].getAttribute("parent") == selected_anchors[i].innerHTML) {
+                    sub_navigation_bars[j].className = "sub-navigation";
+                } else {
+                    sub_navigation_bars[j].className = "sub-navigation hidden";
+                }
+            }
+
+            break;
+        }
+    }
+};
